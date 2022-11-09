18fcdab5105b973843fa6136e7b12aec4f0763f5
chanw12 <wooju9812127@gmail.com>
2022-11-09 13:05:50 +0900

    pwd hash & check phonumber to register

--- harmony/src/pages/signup/signup.jsx
+++ harmony/src/pages/signup/signup.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router";
 import useMutation from "../../libs/useMutation";
-
+import { SHA256 } from "../../libs/useHash";
 const SignUp = (props) => {
   const {
     register,
@@ -22,26 +22,19 @@ const SignUp = (props) => {
   );
   const navigate = useNavigate();
   const onValid = (validData) => {
-    if (isExist !== 1) {
-      console.log("invalid");
-      return;
-    }
-    const phone_number =
-      "010" + validData.signUpPNumber1 + validData.signUpPNumber2;
+    const phone_number = "010" + validData.signUpPNumber;
     const giveData = {
       phone_number,
       user_id: validData.signUpID,
-      password: validData.signUpPassword,
+      password: SHA256(validData.signUpPassword),
     };
     if (loading) return;
     signup(giveData);
   };
   const [isExist, setIsExist] = useState(3); // 서버에 등록된 아이디 조회결과 1:등록 x 2: 등록 o 3:조회 하지않은상태
-  const [idCheckbuttonText, setIdCheckButtonText] = useState("중복확인");
+  const [isExistP, setIsExistP] = useState(3);
   const onInvalid = (inValidData) => {
     console.log(inValidData);
-    console.log("1");
-    console.log("hhh");
   };
   const idCheck = async () => {
     if (watch("signUpID").length < 6) {
@@ -77,6 +70,42 @@ const SignUp = (props) => {
         }
       });
   };
+  const phoneCheck = async () => {
+    if (watch("signUpPNumber").length < 8) {
+      return;
+    }
+    clearErrors("signUpPNumber");
+    const check_data = await fetch(
+      `http://115.85.181.222:8080/member?phone_number=010${watch(
+        "signUpPNumber"
+      )}`,
+      {
+        method: "GET",
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        if (data.data.length > 0) {
+          //아이디가 서버에 존재하는 경우
+          setIsExistP(2);
+          setError("signUpPNumber", {
+            type: "focus",
+            message: "중복된 전화번호 입니다.",
+          });
+        } else {
+          //아이디가 서버에 존재하지 않는 경우
+          setIsExistP(1); //
+          console.log(isExistP);
+          setError("signUpPNumber", {
+            type: "focus",
+            message: "사용 가능한 전화번호 입니다",
+          });
+        }
+      });
+  };
   useEffect(() => {
     if (error) {
       console.log(error);
@@ -88,6 +117,9 @@ const SignUp = (props) => {
   useEffect(() => {
     setIsExist(3);
   }, [watch("signUpID")]);
+  useEffect(() => {
+    setIsExistP(3);
+  }, [watch("signUpPNumber")]);
   return (
     <div className=" px-10 py-10 flex justify-center relative flex-col items-center space-y-4">
       <span className=" font-dancing text-9xl text-pink-400 cursor-default select-none">
@@ -119,7 +151,7 @@ const SignUp = (props) => {
                 type="button"
                 className=" px-2 py-2 w-1/5 text-white font tracking-widest rounded-md bg-pink-300 hover:bg-pink-400"
               >
-                {idCheckbuttonText}
+                중복확인
               </button>
             </div>
           </div>
@@ -184,37 +216,52 @@ const SignUp = (props) => {
               <div className="text-red-500 text-xs font-medium pt-2 pb-5"></div>
             ))}
           <label htmlFor="phoneNumber">전화번호</label>
-          <div id="phoneNumber">
-            <span className=" text-xl">010 - </span>
+          <div
+            id="phoneNumber"
+            className="flex mb-2 space-x-4 items-center justify-center "
+          >
+            <span className=" w-1/5 text-2xl">
+              010&nbsp;&nbsp;&nbsp;&nbsp;-
+            </span>
             <input
               type="number"
-              className=" rounded-md outline-none border-2 p-3 spcae text-xl w-1/5 text-gray-600 h-[30px] text-center"
-              {...register("signUpPNumber1", {
+              className=" rounded-md outline-none border-2 p-5  text-2xl w-3/5 text-gray-600 h-[30px] space-x-2"
+              {...register("signUpPNumber", {
                 required: true,
-                minLength: 4,
-                maxLength: 4,
+                minLength: {
+                  value: 8,
+                  message: "올바른 전화번호 형식이 아닙니다",
+                },
+                maxLength: {
+                  value: 8,
+                  message: "올바른 전화번호 형식이 아닙니다",
+                },
+                validate: {
+                  isExist: () =>
+                    isExist === 2 ? "중복된 전화번호 입니다" : null,
+                  isnotCheck: () => (isExist === 3 ? "중복 확인 필요" : null),
+                },
               })}
               id="tel_1"
             />
-            <span className=" text-2xl"> - </span>
-            <input
-              type="number"
-              className=" rounded-md outline-none border-2 p-3 text-xl text-gray-600 w-1/5  h-[30px] text-center"
-              {...register("signUpPNumber2", {
-                required: true,
-                minLength: 4,
-                maxLength: 4,
-              })}
-              id="tel_2"
-            />
-            {errors.signUpPNumber1 || errors.signUpPNumber2 ? (
-              <div className=" text-red-500 text-xs font-medium pt-2 pb-5">
-                전화번호 형식이 올바르지 않습니다
-              </div>
-            ) : (
-              <div className="text-red-500 text-xs font-medium pt-2 pb-5"></div>
-            )}
+            <button
+              onClick={phoneCheck}
+              className=" w-1/5 px-2 py-2  text-white font tracking-widest rounded-md bg-pink-300 hover:bg-pink-400"
+            >
+              중복확인
+            </button>
           </div>
+          {errors.signUpPNumber ? (
+            <div
+              className={`${
+                isExistP === 1 ? "text-blue-500 " : " text-red-500"
+              } text-xs font-medium pt-2 pb-5`}
+            >
+              {errors.signUpPNumber.message}
+            </div>
+          ) : (
+            <div className="text-red-500 text-xs font-medium pt-2 pb-5"></div>
+          )}
 
           <button className=" hover:bg-pink-400 px-2 py-2 w-full text-white font tracking-widest rounded-md bg-pink-300">
             회 원 가 입
